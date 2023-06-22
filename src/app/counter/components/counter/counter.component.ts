fc25139ec35753be08772b439822fe48c138bf2b
rivera.hidalgo.antonio.jose <rivera.hidalgo.antonio.jose@gmail.com>
2023-06-22 21:18:10 +0200

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>Counter: {{counter}}</h3>
+  <button (click)="increasedBy(-1)">-1</button>
+  <button (click)="reset()">Reset</button>
+  <button (click)="increasedBy(+1)">+1</button>
+`
+})
+
+export class CounterComponent  {
+  public title:string = 'Mi primera app de Angular';
+  public counter:number=10;
+
+  increasedBy(value:number):void{
+    this.counter+=value;
+  }
+
+  reset():void{
+    this.counter=10;
+  }
+}
