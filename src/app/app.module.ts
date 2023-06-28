dc5ce97f9c9364f66890a050a5be777f96932586
rivera.hidalgo.antonio.jose <rivera.hidalgo.antonio.jose@gmail.com>
2023-06-28 17:42:52 +0200

    Fin de seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './heroes/list/list.component';
+
 
 
 
@@ -16,7 +18,8 @@ import { ListComponent } from './heroes/list/list.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
